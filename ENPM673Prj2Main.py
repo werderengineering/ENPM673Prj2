952f99d739b7308e92111c4cccd2073dc4d2d37d
Werder <werdertimothy@gmail.com>
2020-03-01 16:22:19 -0500

    integrated new homography into main

--- ENPM673Prj2Main.py
+++ ENPM673Prj2Main.py
@@ -19,7 +19,7 @@ flag = False
 prgRun = True
 
 def main(prgRun):
-    problem = 2
+    problem = 3
 
     #Correct image
     if problem ==1:
@@ -61,7 +61,7 @@ def main(prgRun):
 
         print("Getting images from " + str(directory))
         imageList = imagefiles(directory)  # get a stack of images
-
+        homo = HomoCalculation.homo_problem2()
         """process each image individually"""
         for i in range(len(imageList)):
             frameDir = directory + '/' + imageList[i]
@@ -76,7 +76,7 @@ def main(prgRun):
             ############################Region ################
             region = process_image(binaryframe)
             #####################Homography and dewarp########################
-            homo = HomoCalculation.homo()
+
             """the next line give you a flat view of current frame"""
             flatfieldBinary = cv2.warpPerspective(region, homo, (frame.shape[0], frame.shape[1]))
             flatBGR= cv2.warpPerspective(frame, homo, (frame.shape[0], frame.shape[1]))
@@ -85,7 +85,7 @@ def main(prgRun):
             ####################Contour#######################################
             cnts, hierarchy = cv2.findContours(flatfieldBinary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             cv2.imshow('flatfield', flatfieldBinary)
-            cntframe = cv2.drawContours(flatBGR, cnts, , (255, 0, 0), 5)
+            cntframe = cv2.drawContours(flatBGR, cnts,-5, (255, 0, 0), 5)
 
             ###################Draw Lines##########################################
             # hist=historgramOnYAxis(grayframe)
@@ -110,39 +110,42 @@ def main(prgRun):
     elif problem == 3:
 
         video = cv2.VideoCapture('challenge_video.mp4')
-
+        homo = HomoCalculation.homo_problem3()
         # Read until video is completed
         while (video.isOpened()):
             # Capture frame-by-frame
             ret, frame = video.read()
             if ret == True:
                 # frame = imutils.resize(frame, width=320, height=180)
-                frame = frame[int(frame.shape[0] / 2) + 70:, :]
+                # frame = frame[int(frame.shape[0] / 2) + 70:, :]
                 ogframe = frame
                 clnframe = frame
                 resetframe = frame
 
                 ##########################Correct frame###########################
                 binaryframe = yellowAndWhite(frame)
-                # frame=binaryframe
-                ############################Histogram Equalization################
+
+                ############################Region ################
                 region = process_image(binaryframe)
+                #####################Homography and dewarp########################
 
-
+                """the next line give you a flat view of current frame"""
+                flatfieldBinary = cv2.warpPerspective(region, homo, (frame.shape[0], frame.shape[1]))
+                flatBGR = cv2.warpPerspective(frame, homo, (frame.shape[0], frame.shape[1]))
 
                 ####################Contour#######################################
+                cnts, hierarchy = cv2.findContours(flatfieldBinary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                cv2.imshow('flatfield', flatfieldBinary)
+                cntframe = cv2.drawContours(flatBGR, cnts, -5, (255, 0, 0), 5)
 
-                #####################Homography and dewarp########################
-                # homo = HomoCalculation.homo()
-                # """the next line give you a flat view of current frame"""
-                # img_unwarped = cv2.warpPerspective(frame, homo, (frame.shape[0], frame.shape[1]))
-                ###################Hough##########################################
+                ###################Draw Lines##########################################
+                # hist=historgramOnYAxis(grayframe)
 
                 ###################Homography and Impose##########################
 
-                cv2.imshow('Original frame', region)
+                cv2.imshow('Original frame', frame)
                 cv2.imshow('Working frame', binaryframe)
-                # cv2.imshow('Flat frame', img_unwarped)
+                cv2.imshow('Flat frame', flatfieldBinary)
 
                 # Press Q on keyboard to  exit
                 if cv2.waitKey(25) & 0xFF == ord('q'):
